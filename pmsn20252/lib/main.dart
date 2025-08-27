fd62d1d1f9b32ff6a377b6ed7d96b082e30bb2f6
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-08-27 16:32:08 -0600

    refactored to statefull widget

--- pmsn20252/lib/main.dart
+++ pmsn20252/lib/main.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:pmsn20252/utils/colors_app.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int contador = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("practica 1"), backgroundColor: Colors.grey),
-        body: Container(child: Center(child: Text("hola mundo"))),
+        body: Container(
+          child: Center(
+            child: Text(
+              "contador $contador",
+              style: TextStyle(
+                fontSize: 25,
+                fontFamily: "Hack",
+                color: ColorsApp.txtColor,
+              ),
+            ),
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(onPressed: miEvento()),
+        // floatingActionButton: FloatingActionButton(onPressed: () {}),
+        // floatingActionButton: FloatingActionButton(onPressed: () => miEvento),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.ads_click),
+          onPressed: () {
+            contador++;
+            print(contador);
+            setState(() {});
+          },
+        ),
       ),
     );
   }
+
+  miEvento() {}
 }
