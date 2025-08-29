de05d59fc498f65a0c06ecc6c22081dd7b036265
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-08-29 16:44:54 -0600

    added text fields and background image at login_screen, WIP: form

---
+++ pmsn20252/lib/screens/login_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController conUser = TextEditingController();
+  TextEditingController conPwd = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final txtUser = TextField(
+      keyboardType: TextInputType.emailAddress,
+      controller: conUser,
+      decoration: InputDecoration(hintText: "Email address"),
+    );
+    final txtPwd = TextField(
+      obscureText: true,
+      controller: conPwd,
+      decoration: InputDecoration(hintText: "Password"),
+    );
+
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        // decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(url))),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/loginBackground.jpg"),
+          ),
+        ),
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned(
+              top: 100,
+              child: Text(
+                "Star wars",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontFamily: 'Jedi',
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 50,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    txtUser,
+                    txtPwd,
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.login, size: 40),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
