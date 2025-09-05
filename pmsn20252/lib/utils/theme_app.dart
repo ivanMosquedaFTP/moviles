47a684e486876df691fd1036a94be2f14048ae47
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-09-05 16:02:56 -0600

    added dark and light themes at theme_app

--- pmsn20252/lib/utils/theme_app.dart
+++ pmsn20252/lib/utils/theme_app.dart
@@ -1,1 +1,37 @@
-class ThemeApp {}
+import 'package:flutter/material.dart';
+
+class ThemeApp {
+  static ThemeData darkTheme() {
+    final theme = ThemeData.dark().copyWith(
+      colorScheme: const ColorScheme(
+        brightness: Brightness.dark,
+        primary: Colors.grey,
+        onPrimary: Colors.amber,
+        secondary: Colors.amber,
+        onSecondary: Colors.red,
+        error: Colors.red,
+        onError: Colors.red,
+        surface: Colors.orange,
+        onSurface: Colors.green,
+      ),
+    );
+    return theme;
+  }
+
+  static lightTheme() {
+    final lightTheme = ThemeData.light().copyWith(
+      colorScheme: const ColorScheme(
+        brightness: Brightness.light,
+        primary: Colors.white,
+        onPrimary: Colors.purple,
+        secondary: Colors.grey,
+        onSecondary: Colors.redAccent,
+        error: Colors.red,
+        onError: Colors.red,
+        surface: Colors.blueAccent,
+        onSurface: Colors.green,
+      ),
+    );
+    return lightTheme;
+  }
+}
