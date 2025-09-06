ddef29667fff603c22877a218f8d3343bf0b1f23
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-09-06 11:32:54 -0600

    fixed hero_widget.dart at /lib

---
+++ pmsn20252/lib/hero_widget.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:pmsn20252/attribute_widget.dart';
+import 'package:pmsn20252/hero_details_page.dart';
+import 'dart:math' as math;
+
+import 'hero_model.dart';
+
+// Degree / Radians converter
+const double degrees2Radians = math.pi / 180.0;
+const double radians2Degrees = 180.0 / math.pi;
+
+double degrees(double radians) => radians * radians2Degrees;
+
+double radians(double degrees) => degrees * degrees2Radians;
+
+class HeroRowWidget extends StatelessWidget {
+  final HeroModel hero;
+  final double rowHeight;
+
+  const HeroRowWidget({required this.hero, this.rowHeight = 282});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: rowHeight,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Transform.translate(
+            offset: Offset(-10, 0),
+            child: Transform(
+              alignment: FractionalOffset.center,
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.01)
+                ..rotateY(radians(1.5)),
+              child: Container(
+                height: 216,
+                margin: EdgeInsets.symmetric(horizontal: 40),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.1),
+                  borderRadius: BorderRadius.all(Radius.circular(22)),
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-44, 0),
+            child: Transform(
+              alignment: FractionalOffset.center,
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.01)
+                ..rotateY(radians(8)),
+              child: Container(
+                height: 188,
+                margin: EdgeInsets.symmetric(horizontal: 40),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.4),
+                  borderRadius: BorderRadius.all(Radius.circular(22)),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Transform.translate(
+                offset: Offset(-30, 0),
+                child: Container(
+                  child: Hero(
+                    tag: hero.name,
+                    child: Image.asset(
+                      hero.image,
+                      width: rowHeight,
+                      height: rowHeight,
+                      package: 'ui_challenge_3',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              margin: EdgeInsets.only(right: 58),
+              padding: EdgeInsets.symmetric(vertical: 34),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  AttributeWidget(
+                    progress: hero.speed,
+                    child: Image.asset(speed, package: 'ui_challenge_3'),
+                  ),
+                  AttributeWidget(
+                    progress: hero.health,
+                    child: Image.asset(heart, package: 'ui_challenge_3'),
+                  ),
+                  AttributeWidget(
+                    progress: hero.attack,
+                    child: Image.asset(knife, package: 'ui_challenge_3'),
+                  ),
+                  SizedBox(
+                    height: 32,
+                    child: OutlinedButton(
+                      child: new Text(
+                        'See Details',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          new MaterialPageRoute(
+                            builder: (context) {
+                              return HeroDetailsPage(hero);
+                            },
+                          ),
+                        );
+                      },
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        side: BorderSide(color: Colors.white, width: 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
