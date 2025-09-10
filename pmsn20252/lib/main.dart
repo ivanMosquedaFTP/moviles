59c448c3d15e681aa1823d2f4eb7c2cbd32533cf
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-09-09 23:05:00 -0600

    added login and register routing, to be fixed

--- pmsn20252/lib/main.dart
+++ pmsn20252/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pmsn20252/screens/home_screen.dart';
 import 'package:pmsn20252/screens/login_screen.dart';
+import 'package:pmsn20252/screens/register_screen.dart';
 import 'package:pmsn20252/ui_challenge_3.dart';
 import 'package:pmsn20252/utils/theme_app.dart';
 import 'package:pmsn20252/utils/value_listener.dart';
@@ -22,9 +23,12 @@ class MyApp extends StatelessWidget {
           routes: {
             "/home": (context) => HomeScreen(),
             "/uiChallenge": (context) => UiChallenge3(),
+            "/register": (context) => RegisterScreen(),
+            "/login": (context) => LoginScreen(),
           },
           title: 'Material App',
-          home: LoginScreen(),
+          // home: LoginScreen(),
+          home: RegisterScreen(),
         );
       },
     );
